e00cf986afaedd8163e1949fc0926eba7cd121f2
anggafsahid <angga.fsahid@gmail.com>
2025-02-08 02:22:04 +0700

    Update Cloudinary page views

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,7 @@
             @foreach ($pages as $page)
                 <a href="{{ route('page.show', $page->slug) }}" class="list-group-item list-group-item-action">
                     <h5>{{ $page->title }}</h5>
-                    <p>{{ \Illuminate\Support\Str::limit($page->content, 150) }}</p>
+                    <p>{{ \Illuminate\Support\Str::limit(strip_tags($page->content), 150) }}</p>
                 </a>
             @endforeach
         </div>
