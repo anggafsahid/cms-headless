ecc331b4db818e58fc19235845de7460d5703f25
anggafsahid <angga.fsahid@gmail.com>
2025-02-08 02:34:45 +0700

    Update Cloudinary just the view

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -6,7 +6,7 @@
 
         <!-- Display Author and Published At in small text below the title -->
         <p class="small text-muted">
-            By {{ $page->author }} | Published on {{ \Carbon\Carbon::parse($page->published_at)->format('F j, Y') }}
+            By {{ $page->author->name }} | Published on {{ \Carbon\Carbon::parse($page->published_at)->format('F j, Y') }}
         </p>
 
         <br>
