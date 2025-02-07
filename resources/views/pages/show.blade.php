4cfe59ca955eb09c861401e4891846eda033af49
anggafsahid <angga.fsahid@gmail.com>
2025-02-08 02:24:27 +0700

    Update Cloudinary API PAGE

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -3,13 +3,21 @@
 @section('content')
     <div class="container">
         <h1>{{ $page->title }}</h1>
+
+        <!-- Display Author and Published At in small text below the title -->
+        <p class="small text-muted">
+            By {{ $page->author }} | Published on {{ \Carbon\Carbon::parse($page->published_at)->format('F j, Y') }}
+        </p>
+
+        <br>
+
         <div class="content">
             {!! $page->content !!}
         </div>
 
         @if ($page->media)
             <div class="media">
-                <img src="{{$page->media}}" alt="Page Media" class="img-fluid">
+                <img src="{{ $page->media }}" alt="Page Media" class="img-fluid">
             </div>
         @endif
     </div>
